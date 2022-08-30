166a7386d29ad7aaeacb248eddae9ba3f90ace0d
mak-p <mak-p@DESKTOP-PG1IP70>
2022-08-30 20:01:51 +0300

    Add project files

--- TransferParameters/main.cpp
+++ TransferParameters/main.cpp
@@ -8,6 +8,7 @@ void main()
 	setlocale(LC_ALL, "");
 	int a = 2, b = 3;
 	cout << a << "\t" << b << endl;
+	cout << &a << "\t" << &b << endl;
 	Exchange(a, b);
 	cout << a << "\t" << b << endl;
 }
@@ -16,4 +17,5 @@ void Exchange(int a, int b)
 	int buffer = a;
 	a = b;
 	b = buffer;
+	cout << &a << "\t" << &b << endl;
 }
